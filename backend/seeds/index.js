04491f2fde312fe38e8162b3e581de7005c44c4b
meetul <meetulrathore98@gmail.com>
2023-04-18 14:16:32 +0530

    campgrounds first seed

---
+++ backend/seeds/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+const Campground = require("../models/campgroundModel");
+const connectDB = require("../config/db");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+
+connectDB();
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
